f28e19c584972b75bd6bcf1c33a156d599f1aa3f
alexandre1828 <144128822+alexandre1828@users.noreply.github.com>
2025-02-18 11:09:40 -0300

    Add files via upload

---
+++ produtos/src/main/java/br/com/api/modelo/RespostaModelo.java
@@ -0,0 +1,15 @@
+package br.com.api.modelo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class RespostaModelo {
+
+    private String mensagem;
+    
+}
---
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -0,0 +1,23 @@
+package br.com.api.produtos.servico;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    // metodo para listar os produtos 
+
+    public Iterable<ProdutoModelo> listar() {
+        return pr.findAll();
+    }
+       
+    
+    
+}
