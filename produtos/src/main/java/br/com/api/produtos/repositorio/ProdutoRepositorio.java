a9bb0f95cef75658495528f11fa733123e4cbf82
alexandre1828 <144128822+alexandre1828@users.noreply.github.com>
2025-02-21 11:25:31 -0300

    Add files via upload

--- produtos/src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
+++ produtos/src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
@@ -3,9 +3,11 @@ package br.com.api.produtos.repositorio;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.api.modelo.ProdutoModelo;
-@Repository
-public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long>{
+import br.com.api.produtos.modelo.ProdutoModelo;
 
+@Repository
+
+public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
     
-} 
+}
+
--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -3,7 +3,7 @@ package br.com.api.produtos.servico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.api.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,12 +12,8 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
-    // metodo para listar os produtos 
-
     public Iterable<ProdutoModelo> listar() {
         return pr.findAll();
     }
-       
-    
     
 }
